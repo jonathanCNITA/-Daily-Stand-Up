f300b76584eab449bccf08ad5c7bba96a3910784
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-07-26 11:37:09 +0200

    Import login component and display on app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './main/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
