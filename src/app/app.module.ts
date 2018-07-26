915f2237c64d84bae017a794f7aecb1aa91d4270
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-07-26 17:01:06 +0200

    Add All needed forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './main/login/login.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule, MatButtonModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
